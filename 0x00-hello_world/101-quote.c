fc8fff294afb7a1c6f0d983be7f6844676da3823
zegedevops <zegetech@outlook.com>
2023-02-19 21:17:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  * main - A c program that prints a line to the standard error
  * Return: 1 (Success)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, "\
-	"2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
