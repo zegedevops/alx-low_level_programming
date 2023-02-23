b610407c6daecd468387c1939e8bfafb2f888b5f
zegedevops <zegetech@outlook.com>
2023-02-23 12:48:00 -0800

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,12 @@
 
 /**
  * _islower - Checks for lowercase character
- * @c: The character to be checked
+ * @c: Character from ASCII to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
-
 {
-
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
