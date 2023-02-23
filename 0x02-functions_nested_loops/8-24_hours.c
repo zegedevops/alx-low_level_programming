2ac1fbff4ad88e2f41076cb3c49fdb492211be2c
zegedevops <zegetech@outlook.com>
2023-02-23 13:34:45 -0800

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,24 +14,18 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-	for (b = 0; b <= 9; b++)
-	{
-	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	for (b = 0; b <= 3; b++)
 	{
 	for (c = 0; c <= 5; c++)
 	{
 	for (d = 0; d <= 9; d++)
-	for (d = 0; d <= (c == 5 ? 4 : 9); d++)
 	{
 	_putchar(a + '0');
 	_putchar(b + '0');
 	_putchar(':');
-	_putchar(d + '0');
 	_putchar(c + '0');
+	_putchar(d + '0');
 	_putchar('\n');
-	if (a == 2 && b == 3 && c == 5 && d == 9)
-		return;
-	}
 	}
 	}
 	}
