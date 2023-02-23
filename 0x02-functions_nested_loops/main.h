a7c416c583be39725d90aed8cd35bef3f239eeec
zegedevops <zegetech@outlook.com>
2023-02-22 20:51:19 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+
+int _puchar(char);
+void print-alphabet(void);
+void print_alphabet_x10(void);
+int_slower(int c);
+int_isalpha(int c);
+int print_sign(int n);
+int_abs(int);
+void jack_bauer(void);
+void times_tale(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
