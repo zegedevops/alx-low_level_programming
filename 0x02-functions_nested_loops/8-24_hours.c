11815754a0c7f7c689c54a86bfb1e3bc45bcdb96
zegedevops <zegetech@outlook.com>
2023-02-23 14:06:23 -0800

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,13 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-	for (b = 0; b <= 3; b++)
+	for (b = 0; b <= 9; b++)
 	{
 	for (c = 0; c <= 5; c++)
 	{
 	for (d = 0; d <= 9; d++)
+	{
+	if (a == 2 && b == 3 && c == 5 && d == 9)
 	{
 	_putchar(a + '0');
 	_putchar(b + '0');
@@ -23,6 +25,7 @@ void jack_bauer(void)
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
+	}
 	}
 	}
 	}
