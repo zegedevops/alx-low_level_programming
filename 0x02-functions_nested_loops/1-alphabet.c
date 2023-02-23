1cb2e46e7a96e416df3a901e6285c377e362ea1c
zegedevops <zegetech@outlook.com>
2023-02-23 00:36:58 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
+	}
+	_putchar('\n');
+}
