d684975804c101c1b7150859d07c6834040944e2
zegedevops <zegetech@outlook.com>
2023-02-23 08:46:58 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower -checks for lowercase character
+ * _islower - Checks for lowercase character
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
@@ -12,7 +12,6 @@ int _islower(int c)
 
 	if (c >= 97 && c <= 122)
 	{
-
 	return (1);
 	}
 	return (0);
