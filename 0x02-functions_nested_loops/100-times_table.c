794f3509eec126fb20045c2e8bbdee555a9366cc
zegedevops <zegetech@outlook.com>
2023-02-24 05:11:18 -0800

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,9 +28,9 @@ void print_times_table(int n)
 	if (prod >= 100)
 	{
 	_putchar((prod / 100) + '0');
-	_putchar((prod / 10)) % 10 + '0');
-	else
-	if (prod <= 99 && prod >= 10)
+	_putchar(((prod / 10)) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
 	{
 	_putchar((prod / 10) + '0');
 	}
@@ -40,4 +40,3 @@ void print_times_table(int n)
 	}
 	}
 }
-
