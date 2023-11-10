7d2aa910689b350ffec8f11087bfc21ce422827d
zegedevops <zegetech@outlook.com>
2023-11-10 06:08:05 +0000

    final correction

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program.
+ *
+ * This program demonstrates the use of an integer array and a pointer.
+ * It initializes an integer array 'a' with 5 elements and assigns the
+ * value 1024 to the element at index 2. It also declares an integer
+ * variable 'n' and a pointer 'p', assigning the address of 'n' to 'p'.
+ *
+ * Return: Always 0 (Success).
+ */
 int main(void)
 {
 	int n;
