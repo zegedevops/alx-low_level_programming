e5eb52ebe5979e552f6b9294832327236d2486d1
zegedevops <zegetech@outlook.com>
2023-11-25 10:51:27 +0000

    0x02 functions & loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @y: int to be added to z
+ * @z: int to be added to y
+ * Return: sum of y and z
+ */
+int add(int y, int z)
+{
+	int a;
+
+	a = y + z;
+
+	return (a);
+}
